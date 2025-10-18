2509e2c7469ff66fe49278158a75ad96f46afe8e
lmfor <leoschub19@gmail.com>
2025-10-18 13:34:43 -0500

    create room logic

--- src/entry.py
+++ src/entry.py
@@ -1,7 +1,32 @@
 from workers import Response, WorkerEntrypoint
-from room import createRoom
+from fastapi import FastAPI, Request, HTTPException
+from pydantic import BaseModel
+
+
 
 class Default(WorkerEntrypoint):
     async def fetch(self, request):
-        return Response(createRoom("abcef"))
+        import asgi
+
+        return await asgi.fetch(app, request, self.env)
     
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return{"message": "root page"}
+
+class Room(BaseModel):
+    room_id: str
+    name: str
+@app.post("/room")
+async def create_room(room: Room, req: Request):
+    env = req.scope["env"]
+    from room import createRoom
+    return await createRoom(env, room.name)
+
+@app.delete("/room/{id}")
+async def delete_room(room: Room, req: Request):
+    env = req.scope["env"]
+    if 
--- src/room.py
+++ src/room.py
@@ -1,3 +1,21 @@
+import random, string
+from pydantic import BaseModel
 
-def createRoom(id):
-    return f"This is a room with id {id}"
+SQL_CREATE_ROOM = "INSERT INTO rooms (room_id, name) VALUES (?, ?) RETURNING room_id, name;"
+
+async def createRoom(env, name):
+    '''
+    try:
+        length = 6
+        room_id = ''.join(random.choices(string.ascii_letters.upper() + string.digits, k=length))
+        # res = await env.DB.prepare(SQL_CREATE_ROOM).bind(room_id, name).all()
+
+    except Exception as e:
+        pass
+    '''
+    length = 6
+    room_id = ''.join(random.choices(string.ascii_letters.upper() + string.digits, k=length))
+
+
+    return {"room_id": room_id, 
+            "name": name}
